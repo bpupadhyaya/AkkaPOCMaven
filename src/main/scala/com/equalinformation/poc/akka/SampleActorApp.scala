904bef21534ad36b993367f5ebe93fc106577ca1
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-12-18 17:36:14 -0800

    Initial project structure.

---
+++ src/main/scala/com/equalinformation/poc/akka/SampleActorApp.scala
@@ -0,0 +1,25 @@
+package com.equalinformation.poc.akka
+
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+/**
+  * Created by bpupadhyaya on 12/18/15.
+  */
+
+class SampleActor extends Actor {
+  def receive = {
+    case "hello" => println("test reflected")
+    case _       => println("not a test")
+  }
+}
+
+object SampleActorApp extends App {
+  val system = ActorSystem("SampleSystem")
+  // default Actor constructor
+  val sampleActor = system.actorOf(Props[SampleActor], name = "testactor")
+  sampleActor ! "test"
+  sampleActor ! "alternate message"
+
+}
---
+++ src/main/scala/com/equalinformation/poc/akka/TestObj.scala
@@ -0,0 +1,9 @@
+package com.equalinformation.poc.akka
+
+/**
+  * Created by bpupadhyaya on 12/18/15.
+  */
+object TestObj extends App {
+  println("test")
+
+}
